00433011f7214c97b69689dd48ae51386088e6dc
Erin Corson <frodopwns@gmail.com>
2013-01-01 16:42:54 -0600

    adding comments

--- unscramble/unscram.py
+++ unscramble/unscram.py
@@ -1,39 +1,40 @@
-import sys
+"""Unscramble the given list of words"""
 
 import gtk
 
+
 def getClipboard():
+    """
+    Returns the text copied from the browser to the clipboard
+    """
     return gtk.Clipboard().wait_for_text()
 
 def setClipboard(text):
+    """
+    Sets a new value to the clipboard for easy pasting
+    """
     cb = gtk.Clipboard()
     cb.set_text(text)
     cb.store()
-
-
   
 def main():
-    
-    if (len(sys.argv) > 1):
-        print 'Options used: ', sys.argv[1]
-        sys.exit()
-        
-    testList = []
+    """
+    Unscramble list of scrambled words from clipboard then cmopare 
+    with list of possible words to find matches.
+    """
+    #open words.txt and append contents to testList
     with open("words.txt", "r") as mywords:
         testList = [x.strip("\r\n") for x in mywords.readlines()]
-        
     scramIn = getClipboard()
+    #split input by tab then strip newlines
     scrambled = [x.strip() for x in scramIn.split("\t")]
-    results = []
     for code in  scrambled:
         codeLen = len(code)
         codeSort = sorted(code)
-
         for word in testList:
             if (len(word) == codeLen):
                 if (sorted(word) == codeSort):
                     results.append(word)
-                    
     setClipboard( ",".join(results))
                 
 
